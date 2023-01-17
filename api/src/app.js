38fa34cf26bf42018d475765e89603863a4cad74
Nicolas <nicoferni55@gmail.com>
2023-01-17 19:35:28 -0300

    test

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,7 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-
+console.log("TEST")
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
